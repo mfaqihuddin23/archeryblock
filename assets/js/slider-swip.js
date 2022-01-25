4469487a8d60f1402d57578f6efeac91973c55d7
unknown <mfaqihuddin23@gmail.com>
2022-01-25 11:54:09 +0700

    logo slide clear

--- assets/js/slider-swip.js
+++ assets/js/slider-swip.js
@@ -1,21 +1,22 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('#sw-1', {
     // Optional parameters
+    slidesPerView:5,
     direction: 'horizontal',
     loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
+    autoplay: {
+      delay: 2000,
+    // reverseDirection: true
+
+    }
+  });
+  const swiper2 = new Swiper('#sw-2', {
+    // Optional parameters
+    slidesPerView:5,
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    reverseDirection: true
+
+    }
   });
