849f50775dc480f42bdc957490c78a9dc37639a5
unknown <mfaqihuddin23@gmail.com>
2022-02-02 13:09:25 +0700

    our partners

--- assets/js/slider-swip.js
+++ assets/js/slider-swip.js
@@ -3,20 +3,38 @@ const swiper = new Swiper('#sw-1', {
     slidesPerView:3,
     direction: 'horizontal',
     loop: true,
-    // autoplay: {
-    //   delay: 2000,
-    // }
+    autoplay: {
+      delay: 2000,
+    }
+  });
+  const swiperb = new Swiper('#sw-1b', {
+    // Optional parameters
+    slidesPerView:5,
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+      delay: 2000,
+    }
   });
   const swiper2 = new Swiper('#sw-2', {
     // Optional parameters
     slidesPerView:3,
     direction: 'horizontal',
     loop: true,
-    // autoplay: {
-    //   delay: 2000,
-    //   reverseDirection: true
-    // },
-    
+    autoplay: {
+      delay: 2000,
+      reverseDirection: true
+    },
+  });
+  const swiper2b = new Swiper('#sw-2b', {
+    // Optional parameters
+    slidesPerView:5,
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+      delay: 2000,
+      reverseDirection: true
+    },
   });
 
   const swiper3 = new Swiper('#sw-3', {
